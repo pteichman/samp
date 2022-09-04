79ce2bff95cf7a2c29d09d19823dac1fe356f346
Peter Teichman <peter@teichman.org>
2022-09-04 08:25:03 -0700

    Update samp with useful features
    Change the max items flag to K from N (to match reservoir
    sampling descriptions online).
    
    Add a flag (-0) for NUL delimited input and output.
    
    Add a flag for seeding the random number generator.

--- samp.go
+++ samp.go
@@ -2,45 +2,99 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
-	"log"
 	"math/rand"
 	"os"
+	"sort"
 	"time"
 )
 
-var num int
-
-func init() {
-	flag.IntVar(&num, "n", 1, "number of lines to sample")
-}
+// samp performs stream sampling on the command line. It's the lossy cat(1)
+// you never knew you needed.
 
 func main() {
-	flag.Parse()
+	fs := flag.NewFlagSet("main", flag.ExitOnError)
+	flag0 := fs.Bool("0", false, "use NUL characters instead of line delimiters")
+	flagK := fs.Int("k", 1, "maximum number of items to pass")
+	flagS := fs.Int64("s", -1, "random seed: -1 for current time")
 
-	var lines []string
+	fs.Parse(os.Args[1:])
 
 	scan := bufio.NewScanner(os.Stdin)
-	for i := 0; i < num; i++ {
-		if !scan.Scan() {
-			log.Fatal("Too few lines in input")
-		}
-
-		lines = append(lines, scan.Text())
+	if *flag0 {
+		scan.Split(splitNulls)
 	}
 
-	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
-	seen := num
+	seed := *flagS
+	if seed < 0 {
+		seed = time.Now().UnixNano()
+	}
+	r := rand.New(rand.NewSource(seed))
+
+	// Fill the reservoir of lines with an initial K items.
+	lines := make([]line, 0, *flagK)
+	for i := 0; i < *flagK; i++ {
+		if scan.Scan() {
+			lines = append(lines, line{i, scan.Bytes()})
+		}
+	}
+
+	// Randomly replace items in the reservoir with the remaining lines.
+	seen := *flagK
 	for scan.Scan() {
 		seen++
 		index := r.Intn(seen)
-		if index < num {
-			lines[index] = scan.Text()
+		if index < len(lines) {
+			lines[index] = line{seen, scan.Bytes()}
 		}
 	}
 
-	for _, line := range lines {
-		fmt.Println(line)
+	// Sort the reservoir to ensure output is ordered like the input.
+	sort.Slice(lines, func(i, j int) bool {
+		return lines[i].num < lines[j].num
+	})
+
+	delim := '\n'
+	if *flag0 {
+		delim = 0
 	}
+
+	// Write the reservoir to stdout.
+	w := bufio.NewWriter(os.Stdout)
+	for _, line := range lines {
+		_, err := w.Write(line.data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Write error: %s", err)
+			os.Exit(1)
+		}
+		_, err = w.WriteRune(delim)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Write error: %s", err)
+			os.Exit(1)
+		}
+	}
+	if err := w.Flush(); err != nil {
+		fmt.Fprintf(os.Stderr, "Write error: %s", err)
+		os.Exit(1)
+	}
+}
+
+type line struct {
+	num  int
+	data []byte
+}
+
+func splitNulls(data []byte, atEOF bool) (int, []byte, error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, 0); i >= 0 {
+		return i + 1, data[0:i], nil
+	}
+	if atEOF {
+		return len(data), data, nil
+	}
+	return 0, nil, nil
 }
